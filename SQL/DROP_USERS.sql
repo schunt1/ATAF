8bd2a98298ef7770730d1e3bd2a8d3589581ee48
schunt1 <schunt1@sky.com>
2016-09-13 21:14:08 +0100

    Drop Script bug

--- SQL/DROP_USERS.sql
+++ SQL/DROP_USERS.sql
@@ -1,4 +1,10 @@
+DECLARE
+  lv_workspace_id NUMBER;
 BEGIN
+  SELECT workspace_id INTO lv_workspace_id
+  FROM apex_workspaces
+  WHERE workspace = 'ATAF';
+  APEX_UTIL.SET_SECURITY_GROUP_ID(p_security_group_id=>lv_workspace_id);
   APEX_UTIL.REMOVE_USER(p_user_name => 'ADMIN_USER');
   APEX_UTIL.REMOVE_USER(p_user_name => 'BASIC_USER');
   APEX_UTIL.DELETE_USER_GROUP(p_group_name => 'ataf_administrator');
