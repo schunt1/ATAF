1101543b9589bd83b37ab0b22e8f1d10d4757903
schunt1 <schunt1@sky.com>
2016-09-13 08:20:37 +0100

    SQL Fixes
    Install changed from txt to sql.  dropScript reformatted.

--- dropScript.sql
+++ dropScript.sql
@@ -1,31 +1,56 @@
-drop TABLE "ATAF_ACTION" cascade constraints /   
-drop TABLE ATAF_DATA cascade constraints /
-drop TABLE ATAF_DATA_GROUPS cascade constraints /
-drop TABLE ATAF_DATA_ITEM cascade constraints /
-drop TABLE ATAF_ITEM cascade constraints /
-drop TABLE ATAF_MODULE cascade constraints /
-drop TABLE ATAF_PROJECT cascade constraints /
-drop TABLE ATAF_SELENIUM cascade constraints /
-drop TABLE ATAF_SPEC_CASE cascade constraints /
-drop TABLE ATAF_SYSTEM_PARAMETERS cascade constraints /
-drop TABLE ATAF_TEST_CASE cascade constraints /
-drop TABLE ATAF_TEST_CASE_IOT cascade constraints /
-drop TABLE ATAF_TEST_COND cascade constraints /
-drop TABLE ATAF_TEST_DATA cascade constraints /
-drop TABLE ATAF_TEST_SPEC cascade constraints /
-drop TABLE ATAF_USER_RESTRICTIONS cascade constraints /
-
-
-drop VIEW ATAF_APEX_APPLICATION_PAGES_V cascade constraints /
-drop VIEW ATAF_APEX_PAGE_ITEMS cascade constraints /
-drop VIEW ATAF_APEX_PAGE_ITEMS_V cascade constraints /
-drop VIEW ATAF_FULL_TEST_DATA_V cascade constraints /
-drop VIEW ATAF_TEST_CONDITION_FULL_V cascade constraints /
-drop VIEW ATAF_TEST_CONDITION_V cascade constraints /
-drop VIEW ATAF_TEST_COND_FULL_V cascade constraints /
-
-drop sequence ATAF_DATA_GROUPS_SEQ / 
-drop sequence ATAF_SEQ /
-
-drop PACKAGE ATAF_DATA_GENERATOR /
-drop PACKAGE ATAF_PKG /
+drop TABLE "ATAF_ACTION" cascade constraints 
+/   
+drop TABLE ATAF_DATA cascade constraints 
+/
+drop TABLE ATAF_DATA_GROUPS cascade constraints 
+/
+drop TABLE ATAF_DATA_ITEM cascade constraints 
+/
+drop TABLE ATAF_ITEM cascade constraints 
+/
+drop TABLE ATAF_MODULE cascade constraints 
+/
+drop TABLE ATAF_PROJECT cascade constraints 
+/
+drop TABLE ATAF_SELENIUM cascade constraints 
+/
+drop TABLE ATAF_SPEC_CASE cascade constraints 
+/
+drop TABLE ATAF_SYSTEM_PARAMETERS cascade constraints 
+/
+drop TABLE ATAF_TEST_CASE cascade constraints 
+/
+drop TABLE ATAF_TEST_CASE_IOT cascade constraints 
+/
+drop TABLE ATAF_TEST_COND cascade constraints 
+/
+drop TABLE ATAF_TEST_DATA cascade constraints 
+/
+drop TABLE ATAF_TEST_SPEC cascade constraints 
+/
+drop TABLE ATAF_USER_RESTRICTIONS cascade constraints 
+/
+drop VIEW ATAF_APEX_APPLICATION_PAGES_V cascade constraints 
+/
+drop VIEW ATAF_APEX_PAGE_ITEMS cascade constraints 
+/
+drop VIEW ATAF_APEX_PAGE_ITEMS_V cascade constraints 
+/
+drop VIEW ATAF_FULL_TEST_DATA_V cascade constraints 
+/
+drop VIEW ATAF_TEST_CONDITION_FULL_V cascade constraints 
+/
+drop VIEW ATAF_TEST_CONDITION_V cascade constraints 
+/
+drop VIEW ATAF_TEST_COND_FULL_V cascade constraints 
+/
+drop sequence ATAF_DATA_GROUPS_SEQ 
+/ 
+drop sequence ATAF_SEQ 
+/
+drop PACKAGE ATAF_DATA_GENERATOR 
+/
+drop PACKAGE ATAF_PKG 
+/
+drop MATERIALIZED VIEW "ATAF_APEX_PAGE_ITEMS_MV" 
+/
