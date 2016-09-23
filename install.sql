c0421173cfcb2c8e7a1fe62591f31ef32f72b5d4
schunt1 <schunt1@sky.com>
2016-09-23 17:08:50 +0100

    Retain Application IDs for self test
    Overwrite existing application and retain dom IDs for self testing.

--- install.sql
+++ install.sql
@@ -57,10 +57,15 @@ BEGIN
     FROM dual;
 	APEX_UTIL.SET_SECURITY_GROUP_ID(p_security_group_id=>l_workspace_id);
     APEX_APPLICATION_INSTALL.set_workspace_id( l_workspace_id );
-    --APEX_APPLICATION_INSTALL.set_application_id(&2);
-    --APEX_APPLICATION_INSTALL.generate_offset;
-    --APEX_APPLICATION_INSTALL.set_schema( l_schema );
-    --APEX_APPLICATION_INSTALL.set_application_alias( 'F' || apex_application.get_application_id );
+    ----------------------------------------
+    -- If not running the ATAF self test  --
+    ----------------------------------------
+    IF '&3' = 'No' THEN 
+      APEX_APPLICATION_INSTALL.set_application_id(&2);
+      APEX_APPLICATION_INSTALL.generate_offset;
+      APEX_APPLICATION_INSTALL.set_schema( l_schema );
+      APEX_APPLICATION_INSTALL.set_application_alias( 'F' || apex_application.get_application_id );
+    END IF;
 END;
 /
 
