cbe87a44574dcd83da7602ab66dfc7543b6e2f02
schunt1 <schunt1@sky.com>
2016-10-10 08:14:37 +0100

    Bug Fix

--- APPLICATION_EXPORTS/ATAF_APPLICATION.sql
+++ APPLICATION_EXPORTS/ATAF_APPLICATION.sql
@@ -27,7 +27,7 @@ prompt APPLICATION 108 - ATAF
 -- Application Export:
 --   Application:     108
 --   Name:            ATAF
---   Date and Time:   07:56 Monday October 10, 2016
+--   Date and Time:   08:14 Monday October 10, 2016
 --   Exported By:     SHUNT
 --   Flashback:       0
 --   Export Type:     Application Export
@@ -95,7 +95,7 @@ wwv_flow_api.create_flow(
 ,p_alias=>nvl(wwv_flow_application_install.get_application_alias,'ATAF')
 ,p_page_view_logging=>'YES'
 ,p_page_protection_enabled_y_n=>'Y'
-,p_checksum_salt_last_reset=>'20161009221443'
+,p_checksum_salt_last_reset=>'20161010081351'
 ,p_bookmark_checksum_function=>'MD5'
 ,p_max_session_length_sec=>28800
 ,p_max_session_idle_sec=>3600
@@ -124,7 +124,7 @@ wwv_flow_api.create_flow(
 ,p_substitution_string_01=>'APP_FAVICONS'
 ,p_substitution_value_01=>'<link rel="shortcut icon" href="#APP_IMAGES#favicon.ico">'
 ,p_last_updated_by=>'SHUNT'
-,p_last_upd_yyyymmddhh24miss=>'20161009221443'
+,p_last_upd_yyyymmddhh24miss=>'20161010081351'
 ,p_file_prefix => nvl(wwv_flow_application_install.get_static_app_file_prefix,'')
 ,p_files_version=>3
 ,p_ui_type_name => null
@@ -14863,7 +14863,7 @@ wwv_flow_api.create_page(
 ,p_cache_mode=>'NOCACHE'
 ,p_help_text=>'No help is available for this page.'
 ,p_last_updated_by=>'SHUNT'
-,p_last_upd_yyyymmddhh24miss=>'20160925171745'
+,p_last_upd_yyyymmddhh24miss=>'20161010081351'
 );
 wwv_flow_api.create_page_plug(
  p_id=>wwv_flow_api.id(67703704269445845778)
@@ -14883,7 +14883,8 @@ wwv_flow_api.create_page_plug(
 '   FOR i',
 '      IN (SELECT sc.test_spec_id,',
 '            sc.test_case_id,',
-'            tc.test_case',
+'            tc.test_case,',
+'            sc.spec_case_id',
 '          FROM ataf_spec_case sc,',
 '            ataf_test_case tc',
 '          WHERE sc.test_case_id = tc.test_case_id',
@@ -14892,7 +14893,7 @@ wwv_flow_api.create_page_plug(
 '   LOOP',
 '      htp.p (',
 '            ''<tr><td><a href="''',
-'         || ''Case_''||i.test_case_id',
+'         || ''Case_''||i.spec_case_id',
 '         || ''.htm">''',
 '         || apex_escape.html(i.test_case)',
 '         || ''</a></td></tr>'');',
