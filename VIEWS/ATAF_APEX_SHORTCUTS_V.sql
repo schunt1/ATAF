a13d08db38138582ecda9268e649d5cbf5261833
Simon Hunt <shunt@Ss-Mac-mini.local>
2017-04-10 18:16:17 +0100

    Bug Fixes

--- VIEWS/ATAF_APEX_SHORTCUTS_V.sql
+++ VIEWS/ATAF_APEX_SHORTCUTS_V.sql
@@ -1,6 +1,5 @@
-CREATE OR REPLACE FORCE VIEW "ATAF_APEX_SHORTCUTS_V" 
-AS
-SELECT 
+CREATE OR REPLACE FORCE VIEW "ATAF_APEX_SHORTCUTS_V" ("WORKSPACE", "WORKSPACE_DISPLAY_NAME", "APPLICATION_ID", "APPLICATION_NAME", "SHORTCUT_NAME", "SHORTCUT_TYPE", "CONDITION_TYPE", "CONDITION_TYPE_CODE", "CONDITION_EXPRESSION1", "CONDITION_EXPRESSION2", "ERROR_TEXT", "BUILD_OPTION", "SHORTCUT", "IS_SUBSCRIBED", "SUBSCRIBED_FROM", "LAST_UPDATED_BY", "LAST_UPDATED_ON", "COMPONENT_COMMENTS", "SHORTCUT_ID", "COMPONENT_SIGNATURE") AS 
+  SELECT 
 --
 --+============================================================================
 --|                     Apex Test Automation Framework
@@ -21,7 +20,7 @@ SELECT
 --| --------------- --------- ------- ------------------------------------------
 --| S. Hunt         09-Apr-17 1       Initial Version                           |
 --+=============================================================================+ 
-* FROM ATAF_AGENT_SHORTCUTS_MV
+"WORKSPACE","WORKSPACE_DISPLAY_NAME","APPLICATION_ID","APPLICATION_NAME","SHORTCUT_NAME","SHORTCUT_TYPE","CONDITION_TYPE","CONDITION_TYPE_CODE","CONDITION_EXPRESSION1","CONDITION_EXPRESSION2","ERROR_TEXT","BUILD_OPTION","SHORTCUT","IS_SUBSCRIBED","SUBSCRIBED_FROM","LAST_UPDATED_BY","LAST_UPDATED_ON","COMPONENT_COMMENTS","SHORTCUT_ID","COMPONENT_SIGNATURE" FROM ATAF_AGENT_SHORTCUTS_MV
 -------------------------------------------------------------------------------
 -- The Apex View are only able to access application elements from the       --
 -- applications parsing schema.  Therefore, you will need to add Agent MVs   --
