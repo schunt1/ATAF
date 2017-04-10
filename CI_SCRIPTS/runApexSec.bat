bf11728e093f7a7ba17f2fda309d8e8f3b63f959
Simon Hunt <shunt@Ss-Mac-mini.local>
2017-04-10 17:07:05 +0100

    ApexSec Run File

---
+++ CI_SCRIPTS/runApexSec.bat
@@ -0,0 +1,4 @@
+echo off
+set apexPwd=%1
+
+java -jar "C:/Program Files/apexsec-win64.jar" --host localhost --sid xe --port 1521 --user apex_050100 --pass apexPwd --savepassword --appid 242 --junit c:\Users\shunt\ApexSec\ATAF.apexsec
