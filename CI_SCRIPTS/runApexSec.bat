41bf44d46bf32346da3fcd35c567f4f0eb5287af
Simon Hunt <shunt@MacBook-Pro.local>
2017-06-30 21:49:36 -0500

    Another Try

--- CI_SCRIPTS/runApexSec.bat
+++ CI_SCRIPTS/runApexSec.bat
@@ -1,4 +1,4 @@
 echo off
 set apexPwd=%1
 
-java -jar "C:/Program Files/apexsec-win64.jar" --host localhost --sid xe --port 1521 --user apex_050100 --pass %apexPwd% --savepassword --appid 242 --junit c:\agent\_work\1\TestResults\ATAF_NEW.apexsec c:\agent\_work\1\TestResults\ATAF.apexsec
+java -jar "C:\Program Files\apexsec-win64.jar" --url http://13.81.247.1:8080/apex/ --workspace ATAF --user ADMIN --pass CanalBoat1! --savepassword  --appid 242 --junit c:\agent\_work\1\s\CI_SCRIPTS\ATAF.apexsec
